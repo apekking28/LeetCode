4f3c152383e77b8d0fa937fadfb3d982af67a495
Ilham firmansyah <106460262+apekking28@users.noreply.github.com>
2023-02-02 01:01:24 +0700

    tribonacci v2

--- tribonacci.js
+++ tribonacci.js
@@ -1,16 +1,23 @@
 var tribonacci = function (n) {
-  if (n == 0) return 0;
-  if (n <= 2) return 1;
-  let a = 0,
-    b = 1,
-    c = 1;
-  for (let i = 3; i < n; i++) {
-    let cur = a + b + c;
-    a = b;
-    b = c;
-    c = cur;
+  // if (n == 0) return 0;
+  // if (n <= 2) return 1;
+  // let a = 0,
+  //   b = 1,
+  //   c = 1;
+  // for (let i = 3; i < n; i++) {
+  //   let cur = a + b + c;
+  //   a = b;
+  //   b = c;
+  //   c = cur;
+  // }
+  // return a + b + c;
+  let series = [0, 1, 1];
+
+  for (let i = 3; i <= n; i++) {
+    let cur = series[i - 1] + series[i - 2] + series[i - 3];
+    series.push(cur);
   }
-  return a + b + c;
+  return series[n];
 };
 
-console.log(tribonacci(2));
+console.log(tribonacci(5));
