97b6e5a7b567c230d316734a043737a0cd41c219
Ilham firmansyah <106460262+apekking28@users.noreply.github.com>
2023-02-19 12:53:26 +0700

    Update defanging-an-IP-address.js

--- defanging-an-IP-address.js
+++ defanging-an-IP-address.js
@@ -1,5 +1,15 @@
 var defangIPaddr = function (address) {
-  return address.split(".").join("[.]");
+  let str = address.split("");
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ".") {
+      str[i] = "[.]";
+      result += str[i];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 };
 
 console.log(defangIPaddr("255.100.50.0"));
