4fe1184d0e877fd229e26273d8d432266770b754
Ilham firmansyah <106460262+apekking28@users.noreply.github.com>
2023-03-14 13:37:34 +0700

    new Question

---
+++ length-of-last-word.js
@@ -0,0 +1,6 @@
+var lengthOfLastWord = function (s) {
+  return s.trim().split(" ").pop().length;
+};
+
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
